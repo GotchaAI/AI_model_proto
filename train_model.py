73c0701cfd36002f523dff8bcfc87d07f63f0f9c
whitecity01 <kh081104@gmail.com>
2025-03-18 23:21:06 +0900

    feat: 제시어 레이블 생성 로직 변경

--- train_model.py
+++ train_model.py
@@ -9,30 +9,36 @@ from sklearn.model_selection import train_test_split
 import config
 
 # ----------------------
-# QuickDraw 데이터 로드 (고양이, 개 각각 10000개)
+# QuickDraw 데이터 로드 
 # ----------------------
 print("QuickDraw 데이터 로딩 중 ...")
-cat_data = QuickDrawDataGroup("cat", max_drawings=config.MAX_DRAWINGS)
-dog_data = QuickDrawDataGroup("dog", max_drawings=config.MAX_DRAWINGS)
-cat_drawings = cat_data.drawings
-dog_drawings = dog_data.drawings
 
-# ----------------------
-# NumPy 변환
-# ----------------------
-X_cat = [np.array(d.get_image(stroke_width=2).resize((config.IMAGE_SIZE[0], config.IMAGE_SIZE[1])).convert("L")) for d in cat_drawings]
-X_dog = [np.array(d.get_image(stroke_width=2).resize((config.IMAGE_SIZE[0], config.IMAGE_SIZE[1])).convert("L")) for d in dog_drawings]
+categories = config.CATEGORIES
 
-X_cat = np.array(X_cat)
-X_dog = np.array(X_dog)
+X_list = []
+y_list = []
 
-# 레이블 생성 (고양이=0, 개=1)
-y_cat = np.zeros(len(X_cat), dtype=np.int32)
-y_dog = np.ones(len(X_dog), dtype=np.int32)
+for label_id, label_name in enumerate(categories):
+    data_group = QuickDrawDataGroup(label_name, max_drawings=config.MAX_DRAWINGS)
+    drawings = data_group.drawings
+    
+    # 이미지 변환
+    images = [
+        np.array(d.get_image(stroke_width=2)
+                 .resize((config.IMAGE_SIZE[0], config.IMAGE_SIZE[1]))
+                 .convert("L"))
+        for d in drawings
+    ]
+    
+    images = np.array(images)
+    labels = np.full(len(images), label_id, dtype=np.int32)
+    
+    X_list.append(images)
+    y_list.append(labels)
 
-# 데이터 합치기
-X = np.concatenate([X_cat, X_dog], axis=0)
-y = np.concatenate([y_cat, y_dog], axis=0)
+X = np.concatenate(X_list, axis=0)
+y = np.concatenate(y_list, axis=0)
+
 
 # 스케일링 및 차원 확장 (CNN 입력용)
 X = X / 255.0
@@ -53,7 +59,7 @@ model = keras.Sequential([
     layers.MaxPooling2D((2,2)),
     layers.Flatten(),
     layers.Dense(64, activation='relu'),
-    layers.Dense(2, activation='softmax')  # 2개의 클래스 (cat/dog)
+    layers.Dense(len(categories), activation='softmax')  # n개의 클래스
 ])
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
@@ -69,4 +75,4 @@ history = model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=con
 # 모델 저장
 # ----------------------
 model.save(config.MODEL_PATH)
-print("모델이 'cat_dog_model.h5' 파일로 저장되었습니다!")
+print("모델이 'picture_model.h5' 파일로 저장되었습니다!")

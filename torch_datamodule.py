3125717a03b646878f632a61c517e9e71ad5ea04
brothergiven <dung3740@gmail.com>
2025-03-19 15:59:46 +0900

    feat: Out Of Memory 방지, 데이터 Lazy Loading

--- torch_datamodule.py
+++ torch_datamodule.py
@@ -7,26 +7,17 @@ import time
 
 # 모든 클래스 넣으면 메모리 터짐
 class QuickDrawAllDataSet(torch.utils.data.Dataset):
-    # 객체 초기화 시 데이터 로드드
+    # 객체 초기화 시 데이터 로드
     def __init__(self, max_drawings=1000, transform=None):
-        self.data = [] 
-        self.labels = [] 
+        self.data_index = [] 
         self.transform = transform if transform else transform.ToTensor() 
         self.max_drawings = max_drawings
         
 
-        o1 = time.time()
-        print("Quickdraw 데이터 로드 중 ...")
         for label_id, label_name in enumerate(config.CATEGORIES): # 각 Label마다
-            print(f" 클래스 [{label_id + 1}]: {label_name} 로드 중 ...")
-            data_group = qd.QuickDrawDataGroup(label_name, max_drawings=max_drawings, recognized=True) # 최대 max_drawings 만큼의 데이터 가져옴
-            for i in range(data_group.drawing_count): # get_drawing: PIL Image 변환환
-                self.data.append(data_group.get_drawing(i)) # 흑백 변환 X, RGB 3채널로
-                self.labels.append(label_id)  # 레이블: 클래스 ID
-            print(f" 클래스 [{label_id + 1}]: {label_name} 로드 완료!")
-        o2 = time.time()
-        print("Quickdraw 데이터 로드 완료.")
-        print(f"{len(config.CATEGORIES)} 개 클래스 {len(self.data)}개 샘플, 실행 시간 : {o2 - o1} 초")
+            data_group = qd.QuickDrawDataGroup(label_name, max_drawings=max_drawings, recognized=True) # 최대 max_drawings 만큼의 데이터 레퍼런스 저장
+            self.data_index.extend([(label_id, i) for i in range(data_group.drawing_count)]) # 아직 데이터 로드 하지 않고 drawing index만 저장장
+
 
     
     def __len__(self):
@@ -38,9 +29,15 @@ class QuickDrawAllDataSet(torch.utils.data.Dataset):
         특정 index의 데이터 반환
         :return: (이미지 Tensor, 정수형 레이블)
         """
-        image = self.data[index]
-        label = self.labels[index]
-        return self.transform(image), label
+        label_id, img_idx = self.data_index[index]
+        label_name = config.CATEGORIES[label_id]
+
+        # LAZY LOADING
+        data_group = qd.QuickDrawDataGroup(label_name, max_drawings=self.max_drawings, recognized=True)
+        image = data_group.get_drawing(img_idx).image
+
+
+        return self.transform(image), label_id
 
 
 
@@ -50,7 +47,7 @@ class QuickDrawDataSet(torch.utils.Dataset):
     def __init__(self, name, max_drawings=1000, transform=None):
         self.index = config.CATEGORIES.index(name) # 해당 클래스의 idx 가져옴
         self.data = []
-
+        self.transform = transform
         print(f" 클래스 [{self.index + 1}]: {name} 로드 중 ...")
         data_group = qd.QuickDrawDataGroup(name, max_drawings=max_drawings, recognized=True) # 최대 max_drawings 만큼의 데이터 가져옴
         for i in range(data_group.drawing_count): # get_drawing: PIL Image 변환
